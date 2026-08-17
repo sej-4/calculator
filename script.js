38cce3a5abde165a574b54e3d90967baf7d7b6b9
Sergio Joya <284065931+sej-4@users.noreply.github.com>
2026-08-17 13:29:40 -0400

    Create operate function

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(operator, firstNumber, secondNumber) {
+  let result;
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "-":
+      return subtract(firstNumber, secondNumber);
+    case "*":
+      return multiply(firstNumber, secondNumber);
+    case "/":
+      return divide(firstNumber, secondNumber);
+    default:
+      return null;
+  }
+}
